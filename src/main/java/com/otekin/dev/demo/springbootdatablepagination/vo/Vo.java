8eaa0e5d997e10d0b8629aba2fad9cf37f3bfc5a
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-09-19 19:03:10 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/dev/demo/springbootdatablepagination/vo/Vo.java
@@ -0,0 +1,15 @@
+package com.otekin.dev.demo.springbootdatablepagination.vo;
+
+import java.util.List;
+
+public class Vo {
+    private List<String[]> data;
+
+    public List<String[]> getData() {
+        return data;
+    }
+
+    public void setData(List<String[]> data) {
+        this.data = data;
+    }
+}
